86d83688a6ec0db57d1a4e373b219f2a4095b3cf
ashenalbum <1049019534@qq.com>
2026-04-27 14:39:02 +0800

    样式修改

--- src/views/cluster/nodes/image-list.vue
+++ src/views/cluster/nodes/image-list.vue
@@ -32,11 +32,11 @@
                         <template #cell="{ record }">
                             <div class="df ai-c">
                                 <span>{{record.Name}}</span>
+                                <a-tooltip :content="'设置为PINNED后，镜像文件不会受到GC影响被自动删除'">
+                                    <icon-lock class="fs-16 ml-6 df-s0 cursor" @click="setDefault(record)" :class="{'default-text':!record.isDefault,'c-orange':record.isDefault,'c-99':!record.isDefault}" />
+                                </a-tooltip>
                                 <a-tooltip content="修改名称">
                                     <icon-edit class="c-blue fs-16 ml-6 cursor df-s0 default-text" @click="openChangeName(record)"></icon-edit>
-                                </a-tooltip>
-                                <a-tooltip :content="'设置为PINNED后，镜像文件不会受到GC影响被自动删除'">
-                                    <icon-lock class="fs-16 ml-6 df-s0 cursor default-text" @click="setDefault(record)" :class="{'c-orange':record.isDefault,'c-99':!record.isDefault}" />
                                 </a-tooltip>
                             </div>
                         </template>
