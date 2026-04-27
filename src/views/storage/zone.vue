ee5851077152de7b95157cd993cd915c7a00f510
ashenalbum <1049019534@qq.com>
2026-04-27 11:30:40 +0800

    fixbug

--- src/views/storage/zone.vue
+++ src/views/storage/zone.vue
@@ -95,6 +95,8 @@
                             <template #cell="{ record }">
                                 <span>{{record.bindstatus||'-'}}</span>
                                 <span v-if="record.state=='attached'&&record.isLock=='true'">（{{ record.lockNodeId }}）</span>
+                                
+                                <span v-if="record.state=='attached'&&record.isLock=='true'" class="c-blue ml-10 cursor default-btn" @click="detach.volumeName=record.volumeName;detach.force=false;submitDetach();">解锁</span>
                             </template>
                         </a-table-column>
 
@@ -111,9 +113,8 @@
                                 <span v-if="hasLonghornSystem && !record.isExpanding" class="c-blue cursor mr-20" @click="openExpend(record)">扩容</span>
                                 <span v-if="record.isExpanding" class="c-blue cursor mr-20" @click="cancelExpand(record)">取消扩容</span>
 
-                                <span v-if="record.state=='detached'" class="c-blue cursor mr-20" @click="openAttach(record)">挂载</span>
+                                <span v-if="record.state=='detached'" class="c-blue cursor mr-20" @click="openAttach(record)">绑定</span>
                                 <span v-if="record.state=='attached'" class="c-blue cursor mr-20" @click="openDetach(record)">分离</span>
-                                <span v-if="record.state=='attached'&&record.isLock=='true'" class="c-blue cursor mr-20" @click="detach.volumeName=record.volumeName;detach.force=false;submitDetach();">解锁</span>
                                 <!-- <span class="c-blue cursor mr-20" v-if="!record.pvDisabled" @click="openPvpvc(record)">创建pv/pvc</span> -->
                                 <a-popconfirm v-if="!record.onlyshow" content="确定要删除吗？" @ok="del(record)" position="lt" >
                                     <span :id="'disk-'+record.name" class="c-blue cursor">删除</span>
@@ -372,7 +373,7 @@ export default {
                         let bindstatus = '';
                         if(obj.state=='attached' && obj.isLock=='true'){ bindstatus = '锁定' }
                         else if(obj.state=='attached'){ bindstatus = '自动' }
-                        else if(obj.state=='detached'){ bindstatus = '未绑定' }
+                        // else if(obj.state=='detached'){ bindstatus = '未绑定' }
 
 
                         return {
