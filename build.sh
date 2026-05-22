d9f5e7c5fae1f03f1fc890637a2ba46a34248775
fykyx521 <446897682@qq.com>
2026-05-22 16:13:22 +0800

    build

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 git pull
 npm run build
-rm -rf ../k8s-offline/assets ../k8s-offline/kui.zip
+rm -rf ../w7panel-server/assets ../w7panel-server/kui.zip
 cd ./dist && zip -r ./kui.zip ./*
-cp ./kui.zip ../../k8s-offline/kodata/
-cd ../../k8s-offline/kodata/ && unzip -o ./kui.zip
+cp ./kui.zip ../../w7panel-server/kodata/
+cd ../../w7panel-server/kodata/ && unzip -o ./kui.zip
 #cp -r ./dist/** ../k8s-offline/kodata/
