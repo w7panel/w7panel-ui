cdc0d8131ade88410ad549bacb49e6eab9218570
fykyx521 <446897682@qq.com>
2026-06-29 15:36:51 +0800

    fix: show tenant menus with wildcard permissions

--- src/views/system/usermanage/index.vue
+++ src/views/system/usermanage/index.vue
@@ -5,11 +5,11 @@
             <a-layout-sider :width="160">
                 <div class="df df-c menu-absolute-div" style="position:absolute;inset:0;overflow:auto;">
                     <a-menu style="width:100%;" v-model:selected-keys="selectMenu" @menu-item-click="handelMicroMenu">
-                        <a-menu-item v-if="permission.includes('usermanage/users')" key="users">用户管理</a-menu-item>
-                        <a-menu-item v-if="permission.includes('usermanage/permission')" key="usermanage-permission">权限套餐</a-menu-item>
-                        <a-menu-item v-if="permission.includes('usermanage/usermanage-whitedomain')" key="usermanage-whitedomain">备案域名</a-menu-item>
-                        <a-menu-item v-if="permission.includes('usermanage/site-setting')" key="usermanage-site-setting">站点设置</a-menu-item>
-                        <a-menu-item v-if="permission.includes('usermanage/usermanage-system')" key="usermanage-system">系统设置</a-menu-item>
+                        <a-menu-item v-if="canMenu('usermanage/users', 'system-user')" key="users">用户管理</a-menu-item>
+                        <a-menu-item v-if="canMenu('usermanage/permission', 'system-permission')" key="usermanage-permission">权限套餐</a-menu-item>
+                        <a-menu-item v-if="canMenu('usermanage/usermanage-whitedomain', 'system-white-domain')" key="usermanage-whitedomain">备案域名</a-menu-item>
+                        <a-menu-item v-if="canMenu('usermanage/site-setting', 'system-site-setting')" key="usermanage-site-setting">站点设置</a-menu-item>
+                        <a-menu-item v-if="canMenu('usermanage/usermanage-system', 'system-system')" key="usermanage-system">系统设置</a-menu-item>
                     </a-menu>
                 </div>
             </a-layout-sider>
@@ -25,6 +25,7 @@
 
 <script>
 import { getPermission } from '@/utils/auth';
+import { hasPermission } from '@/utils/permission-match';
 export default {
     data(){
         return {
@@ -42,6 +43,9 @@ export default {
         },
     },
     methods: {
+        canMenu(path, key){
+            return hasPermission(this.permission, path, key);
+        },
         handelMicroMenu(key){
             this.$router.push({name:key})
         }
