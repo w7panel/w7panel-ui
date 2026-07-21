3a8e153439035c6f9f30ac1e0c5b7004c2c0d899
fykyx521 <446897682@qq.com>
2026-07-21 10:32:07 +0000

    fix: panel.html bug

--- src/views/topapp/micro-container.vue
+++ src/views/topapp/micro-container.vue
@@ -201,6 +201,9 @@ export default{
 
             if(!this.downOk){
                 this.info.frontendUrl = data.proxyUrl;
+                if (this.info.frontendUrl) {
+                    this.info.frontendUrl = this.info.frontendUrl.replace(/\/index\.html$/, '/')
+                }
                 this.downOk = true;
                 panelApi.post(`/static/${this.extra.namespace}/download/${this.extra.name}`)
                 // this.extra.setTimeout = setTimeout(()=>{
