2f95bac9fa3b6fd62c24e3e5c32faab3e688854c
fykyx521 <446897682@qq.com>
2026-08-05 11:28:57 +0800

    fix: 调整应用直达菜单顺序

--- src/views/app/apps/detail.vue
+++ src/views/app/apps/detail.vue
@@ -70,17 +70,17 @@
                         <a-divider v-if="bottomMenus.length || externalServices.length" style="margin:10px;width:auto;min-width:auto;" />
                         <div v-if="$route.name!=''">
                             <a-menu v-if="isHelmPage || ((isMicroPage||isAppDirectPage)&&isHelmApp)" v-model:selected-keys="selectMenu" style="width:100%;" @menu-item-click="changeKey">
-                                <a-menu-item v-if="showAppDirect" key="group-app-direct"><icon-launch />应用直达</a-menu-item>
                                 <a-menu-item key="group-helm-detail" ><icon-apps />应用详情</a-menu-item>
                                 <a-menu-item key="group-helm-domain" ><icon-cloud />域名管理</a-menu-item>
+                                <a-menu-item v-if="showAppDirect" key="group-app-direct"><icon-launch />应用直达</a-menu-item>
                             </a-menu>
                             <a-menu v-else v-model:selected-keys="selectMenu" style="width:100%;" @menu-item-click="changeKey">
-                                <a-menu-item v-if="showAppDirect" key="group-app-direct"><icon-launch />应用直达</a-menu-item>
                                 <a-menu-item key="app-detail-detail"><icon-apps />应用详情</a-menu-item>
                                 <a-menu-item key="app-detail-pod"><icon-nav />容器列表</a-menu-item>
                                  <!-- v-if="permission.includes('app-apps-files')" -->
                                 <a-menu-item v-if="fileeditor" key="app-detail-files"><icon-folder />文件管理</a-menu-item>
                                 <a-menu-item key="app-detail-domain"><icon-cloud />域名管理</a-menu-item>
+                                <a-menu-item v-if="showAppDirect" key="group-app-direct"><icon-launch />应用直达</a-menu-item>
                                 <a-menu-item key="app-detail-job"><icon-code-square />执行脚本</a-menu-item>
                                 <a-menu-item key="app-detail-version"><icon-select-all />历史版本</a-menu-item>
                                 <a-menu-item key="app-detail-moniter"><icon-bar-chart />运行状态</a-menu-item>
